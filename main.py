79b372fd8838bbc02ba7028a888c55113cbe85aa
Brian Hall <bhall7@hawaii.edu>
2014-06-10 14:24:37 -1000

    added read counts table output

--- main.py
+++ main.py
@@ -20,12 +20,22 @@ def main():
         sys.stderr.write("Aww naw, couldn't read oligos.\n")
         exit()
 
-    # Create dictionary to hold counts
+    # Create dictionary to hold counts, lists to hold sample and locus names
     counts = {}
+    samples = []
+    loci = []
 
     for seq in seqs:
         # Get sample name for each sequence
         add_sample_name_from_header(seq)
+        
+        # Skip if no sample name found
+        if not seq.sample:
+            continue
+
+        # Add sample name to list
+        if seq.sample not in samples:
+            samples.append(seq.sample)
 
         # Deprimer each sequence
         sort_seq(oligos, seq)
@@ -34,12 +44,19 @@ def main():
         if not seq.locus:
             continue
 
+        # Add locus name to list
+        if seq.locus not in loci:
+            loci.append(seq.locus)
+
         # Build dictionary of counts of unique reads for each locus/sample
         update_counts_dict(counts, seq)
 
     fastq.close()
     # Write contents of dictionary to stdout
-    print_summary(counts)
+    #print_summary(counts)
+    samples = sorted(samples)
+    loci = sorted(loci)
+    print_read_counts(counts, loci, samples)
 
 def update_counts_dict(counts_dict, seq):
     # dict maps locus to a locus_dict, which maps sample to a seq dict,
@@ -63,6 +80,45 @@ def print_summary(counts_dict):
         for sample, sample_dict in locus_dict.items():
             for seq, count in sample_dict.items():
                 print(locus+"\t"+sample+"\t"+seq+"\t"+str(count))
+
+def print_read_counts(counts_dict, loci, samples):
+    # Print header
+    sys.stdout.write("\t")
+    for sample in samples[:-1]:
+        sys.stdout.write(sample + "\t")
+    sys.stdout.write(samples[-1] + "\n")
+
+    # For each locus, write read counts for each sample
+    for locus in loci:
+        if locus not in counts_dict:
+            write_zero_row(locus, samples)
+        else:
+            write_counts_per_sample(locus, counts_dict[locus], samples)
+
+def write_zero_row(locus, samples):
+    sys.stdout.write(locus + "\t")
+    for sample in samples[:-1]:
+        sys.stdout.write("0\t")
+    sys.stdout.write("0\n")
+
+def write_counts_per_sample(locus, locus_dict, samples):
+    sys.stdout.write(locus + "\t")
+    for sample in samples[:-1]:
+        if sample not in locus_dict:
+            sys.stdout.write("0\t")
+        else:
+            sys.stdout.write(total_reads(locus_dict[sample]) + "\t")
+    # Take care of last sample and newline
+    if samples[-1] not in locus_dict:
+        sys.stdout.write("0\n")
+    else:
+        sys.stdout.write(total_reads(locus_dict[samples[-1]]) + "\n")
+
+def total_reads(sample_dict):
+    total = 0
+    for count in sample_dict.values():
+        total += count
+    return str(count)
 
 
 ###################
